b7fd4dc503e76719b0d36baecc27ec8cf9a63863
harmatinepasztoreva <pasztor.eva91@gmail.com>
2026-04-16 20:59:33 +0200

    Reszponzív navbar és több szintű menü kialakítása, valamint kezdőoldal grid alapú átalakítása
    1. Bettetem az index.phop-ba ezt a plusz szort: "<meta name="viewport" content="width=device-width, initial-scale=1">" --> Ez a viewport meta tag, és ez kell ahhoz, hogy mobilon rendesen működjön a reszponzív nézet és a menü. (PDF 90. oldalán van ez a rész)
    2. Menü css már össze van dolgozva a Stlus.css-el, így az index.php-ból töröltem a menu.css hivatkozást
    3. <nav class="navbar navbar-expand-lg navbar-dark bg-dark"> lett berakva ehelyett: <nav class="navbar navbar-expand-lg custom-nav"> - ez így jobban alkalmazkodik a PDF-hez
    4. "  <a class="navbar-brand" href=".">Menü</a>" menü felírat hozzáadása, mert a PDF mintában is van navbar-brand.
    5. <ul class="navbar-nav "> plusz kiegészítve úy hogy top level, a több szintű menü miatt.
    Több szintű menü alakítása a megoldas.zip-ben leírt feladathoz: navbar, navbar-expand-lg, navbar-toggler, collapse navbar-collapse, navbar-brand	-- forrás: oldal-07.html
    top-level-menu	-- forrás: tobbszintu-menu2.html
    second-level-menu -- Forrás:	tobbszintu-menu2.html
    third-level-menu	-- Forrás: tobbszintu-menu2.html
    többszintű <ul><li><a> szerkezet	-- forrás: tobbszintu-menu2.html
    css módosítása: /* FORRÁS:
       - a second-level-menu / third-level-menu alapja: tobbszintu-menu2.html
       - a mobilos hamburger főmenü működés: oldal-07.html
       - a mobilos position: static és width: 100% a két minta összeillesztése
    */
    6. Kezdőoldal reszponzívvá tétele Bootstrap grid rendszerrel (container, row, col-md-6), két oszlopos elrendezés kialakítása

--- templates/index.tpl.php
+++ templates/index.tpl.php
@@ -4,15 +4,16 @@
 <html>
 <head>
 	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<title><?= $ablakcim['cim'] . ( (isset($ablakcim['mottó'])) ? ('|' . $ablakcim['mottó']) : '' ) ?></title>
 	<link rel="icon" type="image/png" href="./images/f1-kis.png" >
 	<link rel="stylesheet" href="./styles/stilus.css" type="text/css">
-	<link rel="stylesheet" href="./styles/menu.css" type="text/css">
 	<link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@700&display=swap" rel="stylesheet">
 
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+<!-- FORRÁS: Megoldasok\08-Többszintű menü - Bootstrap-Reszponzív-tervezés/2-Bootstrap-reszponzív menü minta, oldal-07.html-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+<!-- FORRÁS: Megoldasok\08-Többszintű menü - Bootstrap-Reszponzív-tervezés/2-Bootstrap-reszponzív menü minta, oldal-07.html-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
 	<?php if(file_exists('./styles/'.$keres['fajl'].'.css')) { ?><link rel="stylesheet" href="./styles/<?= $keres['fajl']?>.css" type="text/css"><?php } ?>
 </head>
@@ -34,15 +35,17 @@
         </div>
         <?php } ?>
     </header>
-
-<nav class="navbar navbar-expand-lg custom-nav">
+<!-- FORRÁS: Megoldasok\08-Többszintű menü - Bootstrap-Reszponzív-tervezés/2-Bootstrap-reszponzív menü minta, oldal-07.html-->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
+        <a class="navbar-brand" href="."><?= $fejlec['cim'] ?></a>
+
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
             <span class="navbar-toggler-icon"></span>
         </button>
         
         <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
+            <ul class="navbar-nav top-level-menu">
                 <?php foreach ($oldalak as $url => $oldal) { 
                     if(isset($_SESSION['login']) && $url == 'belepes') continue;
                     if(!isset($_SESSION['login']) && $url == 'kilepes') continue;
@@ -50,43 +53,53 @@
                     
                     $hasSubmenu = isset($oldal['almenu']);
                 ?>
-                    <li class="nav-item <?= $hasSubmenu ? 'dropdown' : '' ?>">
-                        <a class="nav-link <?= $hasSubmenu ? 'dropdown-toggle' : '' ?>" 
-                           href="<?= ($url == '/') ? '.' : ('?oldal=' . $url) ?>"
-                           <?= $hasSubmenu ? 'data-bs-toggle="dropdown" data-bs-auto-close="outside"' : '' ?>>
-                            <?= $oldal['szoveg'] ?>
-                        </a>
-                        
-                        <?php if ($hasSubmenu) { ?>
-                            <ul class="dropdown-menu">
-                                <?php foreach($oldal['almenu'] as $alurl => $aloldal) { 
-                                    $hasThirdLevel = isset($aloldal['almenu']);
-                                ?>
-                                    <li class="<?= $hasThirdLevel ? 'dropend' : '' ?>">
-                                        <a class="dropdown-item <?= $hasThirdLevel ? 'dropdown-toggle' : '' ?>" 
-                                           href="?oldal=<?= $alurl ?>"
-                                           <?= $hasThirdLevel ? 'data-bs-toggle="dropdown"' : '' ?>>
-                                            <?= $aloldal['szoveg'] ?>
-                                        </a>
-                                        <?php if ($hasThirdLevel) { ?>
-                                            <ul class="dropdown-menu">
-                                                <?php foreach($aloldal['almenu'] as $harmadikUrl => $harmadikOldal) { ?>
-                                                    <li><a class="dropdown-item" href="?oldal=<?= $harmadikUrl ?>"><?= $harmadikOldal['szoveg'] ?></a></li>
-                                                <?php } ?>
-                                            </ul>
-                                        <?php } ?>
-                                    </li>
-                                <?php } ?>
-                            </ul>
-                        <?php } ?>
-                    </li>
-                <?php } ?>
-            </ul>
-        </div>
-    </div>
-</nav>
+                        <!-- ÚJ: has-submenu osztály -->
+                        <!-- FORRÁS: tobbszintu-menu2.html logikája -->
+                        <li class="nav-item <?= $hasSubmenu ? 'has-submenu' : '' ?>">
+                            <a class="nav-link" href="<?= ($url == '/') ? '.' : ('?oldal=' . $url) ?>">
+                                <?= $oldal['szoveg'] ?>
+                            </a>
+                            
+                            <?php if ($hasSubmenu) { ?>
+                                <!-- ÚJ: second-level-menu -->
+                                <!-- FORRÁS: tobbszintu-menu2.html -->
+                                <ul class="second-level-menu">
+                                    <?php foreach($oldal['almenu'] as $alurl => $aloldal) { 
+                                        $hasThirdLevel = isset($aloldal['almenu']);
+                                    ?>
+                                        <!-- ÚJ: has-submenu osztály a 2. szinten -->
+                                        <!-- FORRÁS: tobbszintu-menu2.html logikája -->
+                                        <li class="<?= $hasThirdLevel ? 'has-submenu' : '' ?>">
+                                            <a href="?oldal=<?= $alurl ?>">
+                                                <?= $aloldal['szoveg'] ?>
+                                            </a>
 
-    <div id="wrapper"> <div id="content">
+                                            <?php if ($hasThirdLevel) { ?>
+                                                <!-- ÚJ: third-level-menu -->
+                                                <!-- FORRÁS: tobbszintu-menu2.html -->
+                                                <ul class="third-level-menu">
+                                                    <?php foreach($aloldal['almenu'] as $harmadikUrl => $harmadikOldal) { ?>
+                                                        <li>
+                                                            <a href="?oldal=<?= $harmadikUrl ?>">
+                                                                <?= $harmadikOldal['szoveg'] ?>
+                                                            </a>
+                                                        </li>
+                                                    <?php } ?>
+                                                </ul>
+                                            <?php } ?>
+                                        </li>
+                                    <?php } ?>
+                                </ul>
+                            <?php } ?>
+                        </li>
+                    <?php } ?>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div id="wrapper"> 
+        <div id="content">
             <?php include("./templates/pages/{$keres['fajl']}.tpl.php"); ?>
         </div>
         
@@ -95,5 +108,6 @@
             &nbsp;
             <?php if(isset($lablec['ceg'])) { ?><?= $lablec['ceg']; ?><?php } ?>
         </footer>
-    </div> </body>
+    </div> 
+</body>
 </html>
