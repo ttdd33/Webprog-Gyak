7f1e41e5e8f0a5d6c88c5859f8fcd8b70d8a425f
harmatinepasztoreva <pasztor.eva91@gmail.com>
2026-04-18 09:59:15 +0200

    Képek menüpont hozzáadása és képek templet oldal létrehozva, Képek mappa létrehozva

--- includes/config.inc.php
+++ includes/config.inc.php
@@ -40,7 +40,8 @@ $oldalak = array(
     )),
 
     'bemutatkozas' => array('fajl' => 'bemutatkozas', 'szoveg' => 'Bemutatkozás'),
-	'kapcsolat' => array('fajl' => 'kapcsolat', 'szoveg' => 'Kapcsolat'),
+	'kepek' => array('fajl' => 'kepek', 'szoveg' => 'Képek'),
+    'kapcsolat' => array('fajl' => 'kapcsolat', 'szoveg' => 'Kapcsolat'),
 	'cikkek' => array('fajl' => 'cikkek', 'szoveg' => 'Cikkek'),
     'tablazat' => array('fajl' => 'tablazat', 'szoveg' => 'Táblázat'),
     'belepes' => array('fajl' => 'belepes', 'szoveg' => 'Belépés'),
---
+++ templates/pages/kepek.tpl.php
@@ -0,0 +1,8 @@
+ <!-- Forrás: Megoldasok\11-PHP - képek, galéria, képfeltöltés-Web-alkalmazások biztonsága\Képek, galéria, képfeltöltés - config.inc
+<?php
+    $MAPPA = './kepek/';
+    $TIPUSOK = array ('.jpg', '.png');
+    $MEDIATIPUSOK = array('image/jpeg', 'image/png');
+    $DATUMFORMA = "Y.m.d. H:i";
+    $MAXMERET = 500*1024;
+?>
